1265e995d5b430ea6c7b196549bcd9fc28e45930
daihani <m_aldaihani@hotmail.com>
2022-06-25 19:47:51 +0300

    hm2.1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var favoriteNumber = 5
+var name2 = "Mohammad"
+var hobby = "Coding"
+
+print("Hello my name is \(name2). My favorite number is \(favoriteNumber). My hobby is \(hobby).")
